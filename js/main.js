ee78027e1bf68a982a29f88eb972ee9451789719
ShiroDRey <itauch222@list.ru>
2024-06-20 11:33:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//let btn = document.getElementById("button")
+let language = "ru-RU"
+
+function pay() {
+  var widget = new cp.CloudPayments({
+    language: language
+  })
+
+  widget.pay('auth', // или 'charge'
+    { //options
+      publicId: 'pk_36321942fc3d1ade1ea0804adcf77', //id из личного кабинета
+      description: document.getElementById(description), //назначение
+      amount: parseFloat(document.getElementById(amount)), //сумма
+      currency: 'RUB', //валюта
+      accountId: document.getElementById(email), //идентификатор плательщика (необязательно)
+    }, {
+      onSuccess: function(options) { // success
+        //действие при успешной оплате
+      },
+      onFail: function(reason, options) { // fail
+        //действие при неуспешной оплате
+      },
+      onComplete: function(paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
+        //например вызов вашей аналитики Facebook Pixel
+      }
+    }
+  )
+}
+
+//window.addEventListener('load', pay)
+window.onload =function(){
+    let btn = document.getElementById("button")
+    btn.addEventListener('click', pay)
+}
