8396dbb4fdf42e2ea7f6153fb4f383fdf28c7e26
ShiroDRey <itauch222@list.ru>
2024-06-20 12:59:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,14 +5,29 @@ function pay() {
   var widget = new cp.CloudPayments({
     language: language
   })
+let data = {
+        firstName: document.getElementById('firstname').value,
+        lastName: document.getElementById('lastname').value,
+        phone: document.getElementById('phone').value
+    }
 
+    let recurrent = document.getElementById('recurrent')
+    if (recurrent.checked) {
+        data.CloudPayments = {
+            recurrent: {
+                interval: 'Day',
+                period: 1
+            } //один раз в месяц начиная со следующего месяца
+        }
+    }
   widget.pay('auth', // или 'charge'
     { //options
       publicId: 'pk_36321942fc3d1ade1ea0804adcf77', //id из личного кабинета
-      description: document.getElementById(description), //назначение
-      amount: parseFloat(document.getElementById(amount)), //сумма
+      description: document.getElementById(description).value, //назначение
+      amount: parseFloat(document.getElementById(amount).value), //сумма
       currency: 'RUB', //валюта
-      accountId: document.getElementById(email) //идентификатор плательщика (необязательно)
+      accountId: document.getElementById(email).value, //идентификатор плательщика (необязательно)
+      email: document.getElementByID(email).value
     }, {
       onSuccess: function(options) { // success
         //действие при успешной оплате
